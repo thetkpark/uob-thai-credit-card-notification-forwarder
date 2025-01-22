5ae77a7b03ca3accf40c6abfbf3267e3aaf240d4
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2025-01-22 09:55:30 +0700

    add echo

--- notifier/notification/telegram.go
+++ notifier/notification/telegram.go
@@ -2,7 +2,9 @@ package notification
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
 	"log"
 	"log/slog"
 )
@@ -13,12 +15,27 @@ type TelegramNotification struct {
 }
 
 func NewTelegramNotification(token string, targetChatIds []string) *TelegramNotification {
-	b, err := bot.New(token)
+	b, err := bot.New(token, bot.WithDefaultHandler(botDefaultHandler))
 	if err != nil {
 		slog.Error("Error creating telegram bot", slog.String("error", err.Error()))
 		log.Fatalln("Error creating telegram bot")
 	}
+	b.Start(context.Background())
 	return &TelegramNotification{b: b, targetChatIds: targetChatIds}
+}
+
+func botDefaultHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	if update.Message.Text != "/echo" {
+		return
+	}
+
+	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   fmt.Sprintf("Your chat id is: %d", update.Message.Chat.ID),
+	})
+	if err != nil {
+		slog.Error("Error sending message", slog.String("error", err.Error()))
+	}
 }
 
 func (t TelegramNotification) BroadcastNotification(ctx context.Context, msg string) error {
